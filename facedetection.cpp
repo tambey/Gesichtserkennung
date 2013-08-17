7ca1203fac75de3c84912dc226bec74d126a0288
tambey <tambey-01@windowslive.com>
2013-08-17 11:51:15 +0200

    Update facedetection.cpp

--- facedetection.cpp
+++ facedetection.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <stdio.h>
 
- #include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
